b9a16bc25c089b99a00c0bcdae42ed49240e86a2
Anthony Lackey <alackey96@gmail.com>
2021-12-01 20:37:17 -0800

    Clean up day1 code

--- day1/day1.go
+++ day1/day1.go
@@ -7,54 +7,52 @@ import (
 	"strconv"
 )
 
-func getInput() (*bufio.Scanner, error) {
+func parseInput() ([]int, error) {
 	file, err := os.Open("day1/input.txt")
 	if err != nil {
 		return nil, err
 	}
-	return bufio.NewScanner(file), nil
-}
-
-// Part1 counts the number of times a depth measurement increases from
-// the previous measurement
-func Part1() (int, error) {
-	scanner, err := getInput()
-	if err != nil {
-		return 0, err
-	}
-
-	var count int
-	prev := math.MaxInt
-
-	for scanner.Scan() {
-		num, err := strconv.Atoi(scanner.Text())
-		if err != nil {
-			return 0, err
-		}
-
-		if num > prev {
-			count++
-		}
-		prev = num
-	}
-	return count, nil
-}
-
-// Part2 counts the number of times the sum of measurements in this sliding
-// window increases from the previous sum
-func Part2() (int, error) {
-	scanner, err := getInput()
-	if err != nil {
-		return 0, err
-	}
+	scanner := bufio.NewScanner(file)
 
 	var nums []int
 	for scanner.Scan() {
 		num, err := strconv.Atoi(scanner.Text())
 		if err != nil {
-			return 0, err
+			return nil, err
 		}
 		nums = append(nums, num)
+	}
+	return nums, nil
+}
+
+func finalCount(nums []int) int {
+	var count int
+	prev := math.MaxInt
+	for _, v := range nums {
+		if v > prev {
+			count++
+		}
+		prev = v
+	}
+	return count
+}
+
+// Part1 counts the number of times a depth measurement increases from
+// the previous measurement
+func Part1() (int, error) {
+	nums, err := parseInput()
+	if err != nil {
+		return 0, err
+	}
+	return finalCount(nums), nil
+}
+
+// Part2 counts the number of times the sum of measurements in this sliding
+// window increases from the previous sum
+func Part2() (int, error) {
+	nums, err := parseInput()
+	if err != nil {
+		return 0, err
 	}
 
 	var sums []int
@@ -62,14 +60,5 @@ func Part2() (int, error) {
 		sum := nums[i] + nums[i+1] + nums[i+2]
 		sums = append(sums, sum)
 	}
-
-	var count int
-	prev := math.MaxInt
-	for _, v := range sums {
-		if v > prev {
-			count++
-		}
-		prev = v
-	}
-	return count, nil
+	return finalCount(sums), nil
 }
